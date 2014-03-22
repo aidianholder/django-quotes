c5a90d7de5d31bb865c6c85b3e204440936f0c3d
aidian holder <aidianholder@yahoo.com>
2014-03-22 13:22:00 -0700

    fixed changequote.py

--- management/commands/changequote.py
+++ management/commands/changequote.py
@@ -11,11 +11,12 @@ class Command(NoArgsCommand):
 		active_quote = Quote.objects.get(active=True)
 		count = fresh_quotes.count()
 		random_index = randint(0, count-1)
+		active_quote.active=False
+		active_quote.save()
 		new_quote = fresh_quotes[random_index]
 		new_quote.active=True
 		new_quote.save()
-		active_quote.active=False
-		active_quote.save()
+		
 
 
 
